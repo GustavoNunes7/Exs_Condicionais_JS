a50302ee832afac4814b22f644745ffe437acf35
Gustavo Nunes <Gununes.tech7@gmail.com>
2026-02-23 15:30:46 -0300

    Desafio

--- Condicionais.js
+++ Condicionais.js
@@ -183,9 +183,9 @@ let falta = 19
 if(media <= 5 || falta >= 20){
     console.log("Reprovado ❌")
 } else if (media >= 9 && falta <= 5){
-    console.log("Aprovado com excelência !!!!")
+    console.log("Aprovado com excelência ✅👏 !!!!")
 } else if (media >= 7){
-    console.log("Aprovado")
+    console.log("Aprovado ✅")
 }else if (media >= 7 && falta >= 20){
     console.log("Reprovado ❌")
 } else if (media < 6.9 && falta <= 20){
