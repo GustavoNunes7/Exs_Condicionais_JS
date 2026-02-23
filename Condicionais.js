6f0aff9589ad899b254a80b00077261d9d5950a3
Gustavo Nunes <Gununes.tech7@gmail.com>
2026-02-23 15:29:01 -0300

    Desafio

--- Condicionais.js
+++ Condicionais.js
@@ -170,3 +170,24 @@ if(semaforo == "verde"){
 } else{
     console.log("Pare 🔴")
 }
+
+
+
+//DESAFIO
+let media = 6.4
+let falta = 19
+
+
+if(media <= 5 || falta >= 20){
+    console.log("Reprovado ❌")
+} else if (media >= 9 && falta <= 5){
+    console.log("Aprovado com excelência !!!!")
+} else if (media >= 7){
+    console.log("Aprovado")
+}else if (media >= 7 && falta >= 20){
+    console.log("Reprovado ❌")
+} else if (media < 6.9 && falta <= 20){
+    console.log("Recuperação 😐")
+} else if (media < 6.9 && falta >= 20){
+    console.log("Reprovado ❌")
+} 
